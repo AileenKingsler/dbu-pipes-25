e0c5190e08705df4f5e5d6dac3be9f4a09f06d68
Алёна Царегородцева <aileen.kingsler@mail.ru>
2022-03-10 18:30:45 +1000

    Works

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,9 @@
 $(function () {
 
+  $('.works-slider').slick({
+    prevArrow: '<button type="button" class="slick-prev btn btn-secondary"><div class="sr-only">Previous</div><svg width="13" height="23" viewBox="0 0 13 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 1L2 11.5L12 22" stroke="currentColor" stroke-width="2"/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next btn btn-secondary"><div class="sr-only">Previous</div><svg width="13" height="23" viewBox="0 0 13 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 22L11 11.5L0.999998 1" stroke="currentColor" stroke-width="2"/></svg></button>',
+    adaptiveHeight: true,
+  });
+
 });
