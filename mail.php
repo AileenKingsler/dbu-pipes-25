51c6f3778d7cb8c256692f1ecdae7ed47fae7906
Алёна Царегородцева <aileen.kingsler@mail.ru>
2022-03-18 14:39:40 +1000

    Updates

--- mail.php
+++ mail.php
@@ -5,10 +5,14 @@ $subject = "Сообщение с сайта ДБУ";
 
 $name = htmlspecialchars($_POST["name"]);
 $phone = htmlspecialchars($_POST["phone"]);
+$question = htmlspecialchars($_POST["question"]);
 
 $mess = "Телефон: $phone";
 if ($name) {
   $mess .= "\r\nИмя: $name";
+}
+if ($question) {
+  $mess .= "\r\nВопрос: $question";
 }
 
 mail($address, $subject, $mess);
